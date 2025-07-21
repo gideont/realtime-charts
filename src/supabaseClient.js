d54611e40f3b2a6584a55ca2319698adcd079e93
Gideon Tsang <gideon.tsang@digitaledgedc.com>
2025-07-21 03:11:29 -0700

    Editing settings

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://yhbeqyppvyukvmwqjqbo.supabase.co';
-const supabaseKey = 'sb_publishable_ORNOgcK_jhZ6eL0H9BMd1Q_piEe1cFr';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
